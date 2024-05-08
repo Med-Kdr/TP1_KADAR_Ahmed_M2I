1bf178d69c8ec02e8588ad2b7bbc6109ed2e0661
Hmed <hmedcls63s@gmail.com>
2024-05-08 14:57:01 +0100

    Initial commit

---
+++ src/main/java/com/example/tp1_kadar_ahmed_m2i/Exercice1/Table_multiplication.java
@@ -0,0 +1,2 @@
+package com.example.tp1_kadar_ahmed_m2i.Exercice1;public class Table_multiplication {
+}
---
+++ src/main/java/com/example/tp1_kadar_ahmed_m2i/Exercice2/Formulaire.java
@@ -0,0 +1,2 @@
+package com.example.tp1_kadar_ahmed_m2i.Exercice2;public class Formulaire {
+}
